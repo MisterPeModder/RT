98c07c2abad26da7e9fe022e55f1b763cd36d2da
yguaye <yguaye@student.42.fr>
2018-05-29 13:59:33 +0200

    Improve GCC compatibility

--- srcs/ocl_render.c
+++ srcs/ocl_render.c
@@ -6,10 +6,11 @@
 /*   By: jhache <jhache@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/27 22:08:53 by jhache            #+#    #+#             */
-/*   Updated: 2018/05/29 13:50:08 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/05/29 13:59:02 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <libft_base/memory.h>
 #include <libft_base/io.h>
 #include <unistd.h>
 #include "rt.h"
@@ -20,8 +21,13 @@ static cl_mem		ocl_set_kernel_arg(t_rt *core)
 {
 	cl_int			ret;
 	cl_mem			tmp[4];
-	cl_image_format	format = {{CL_RGBA, CL_UNSIGNED_INT8}};
-	cl_image_desc	desc = {CL_MEM_OBJECT_IMAGE2D, core->frame->w, core->frame->h, 1, 1, 0, 0, 0, 0, NULL};
+	cl_image_format	format = (cl_image_format){.image_channel_order = CL_RGBA,
+		.image_channel_data_type = CL_UNSIGNED_INT8};
+	cl_image_desc	desc = (cl_image_desc){.image_type = CL_MEM_OBJECT_IMAGE2D,
+		.image_width = core->frame->w, .image_height = core->frame->h,
+		.image_depth = 1, .image_array_size = 1, .image_row_pitch = 0,
+		.image_slice_pitch = 0, .num_mip_levels = 0, .num_samples = 0,
+		.buffer = NULL};
 
 	tmp[0] = clCreateImage(core->ocl.context, CL_MEM_WRITE_ONLY | CL_MEM_ALLOC_HOST_PTR, &format, &desc, NULL, &ret);
 	if (ret != CL_SUCCESS)
@@ -62,6 +68,7 @@ void				render_frame(t_rt *core)
 	cl_mem			tmp;
 	size_t			work_dim[3];
 	cl_int			ret;
+	size_t			offsets[3];
 
 	if (!(tmp = ocl_set_kernel_arg(core)))
 	{
@@ -72,7 +79,7 @@ void				render_frame(t_rt *core)
 	work_dim[1] = core->frame->h;
 	work_dim[2] = 1;
 	ret = clEnqueueNDRangeKernel(core->ocl.queue, core->ocl.kernel, 2, NULL, work_dim, NULL, 0, NULL, NULL);
-	size_t	aa[] = {0, 0, 0};//on ne peut pas mettre NULL a la place
-	ret = clEnqueueReadImage(core->ocl.queue, tmp, CL_TRUE, aa,
+	ft_bzero(offsets, sizeof(size_t) * 3);
+	ret = clEnqueueReadImage(core->ocl.queue, tmp, CL_TRUE, offsets,
 			work_dim, 0, 0, core->frame->data, 0, NULL, NULL);
 }
