3e4704398d625e5f2e615e33db82a21547958fee
yguaye <yguaye@student.42.fr>
2018-06-13 16:22:44 +0200

    Timer fix

--- srcs/timer.c
+++ srcs/timer.c
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 16:16:26 by yguaye            #+#    #+#             */
-/*   Updated: 2018/05/31 19:30:20 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/06/13 16:22:22 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void				timer_start(t_timer *timer)
 
 	gettimeofday(&start, NULL);
 	timer->val = start.tv_sec * 1000000 + start.tv_usec;
+	timer->secs = 0;
+	timer->msecs = 0;
 }
 
 void				timer_end(t_timer *timer)
@@ -39,12 +41,28 @@ float				timer_span(t_timer *timer)
 	return ((float)timer->secs + (float)timer->msecs / 100000.f);
 }
 
+static void			put_microseconds(int nbr)
+{
+	int				buff[6];
+	int				i;
+
+	i = 6;
+	while (--i >= 0)
+	{
+		buff[i] = nbr % 10;
+		nbr /= 10;
+	}
+	i = 0;
+	while (i < 6)
+		ft_putchar(buff[i++] + '0');
+}
+
 void				timer_display(t_timer *timer, const char *msg)
 {
 	if (msg)
 		ft_putstr(msg);
 	ft_putnbr(timer->secs);
 	ft_putchar('.');
-	ft_putnbr(timer->msecs);
+	put_microseconds(timer->msecs);
 	ft_putchar('\n');
 }
